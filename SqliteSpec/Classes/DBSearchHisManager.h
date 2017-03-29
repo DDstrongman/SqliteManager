2c2898697e6e6c56440efb48b1651d7cc4c613ef
sanli <lishengshu@xinxindai.com>
2017-03-29 15:28:48 +0800

    modify 1.0

---
+++ SqliteSpec/Classes/DBSearchHisManager.h
@@ -0,0 +1,109 @@
+//
+//  DBManager.h
+//  YZDoctors
+//
+//  Created by lishengshu on 15-7-29.
+//  Copyright (c) 2015年 李胜书. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FMDB.h"
+
+@interface DBSearchHisManager : NSObject
+
++ (DBSearchHisManager *)ShareInstance;
+
+@property (nonatomic,strong)FMDatabase *searchHisDB;
+
+#pragma 新的设计将要用到的函数
+
+/**
+ 创建数据库的函数，利用FMDB的SQL语句创造本地数据库
+
+ @param dbName 本地数据库名
+ @return 返回是否成功的bool
+ */
+- (BOOL)creatDatabase:(NSString *)dbName;
+/**
+ 数据库是否打开
+
+ @return 返回结果
+ */
+- (BOOL)isDBReady;
+/**
+ 检查本地数据库表是否存在，如不存在，以数组作为键名建表，默认数组第一个元素作为主键
+
+ @param tName 表名
+ @param keyArr 数据库参数数组,数组第一个元素是主键名
+ @return 返回是否成功的bool
+ */
+- (BOOL)isTableExist:(NSString *)tName TKeyArr:(NSArray *)keyArr;
+
+/**
+ 根据字典添加数据入库，如果不存在，则新增，如果已存在，更新数据库
+
+ @param tableName 表名
+ @param dataDic 数据的字典
+ @return 返回结果bool，成功与否
+ */
+- (BOOL)addTableObj:(NSString *)tableName DataDic:(NSDictionary *)dataDic;
+/**
+ 获取某一表的某一数值的记录
+
+ @param tableName 表名
+ @[aram sqlKeyWord sql里的字段名
+ @param searchKeyWords 搜索的关键字
+ @return 返回的结果
+ */
+- (FMResultSet *)SearchOne:(NSString *)tableName SQLKeyWord:(NSString *)sqlKeyWord SearchKeyWords:(NSString *)searchKeyWords;
+/**
+ 返回某一表的最后10行记录
+
+ @param tableName 表名
+ @return 返回的记录
+ */
+- (FMResultSet *)SearchLastTen:(NSString *)tableName;
+/**
+ 搜索并返回某一表的所有字段数值
+
+ @param tableName 要获取的表名
+ @return 返回的FMDB的特有格式数组
+ */
+- (FMResultSet *)SearchAll:(NSString *)tableName;
+/**
+ 获取当前打开的本地数据库的所有表名，返回数组
+
+ @return 返回的表名数组
+ */
+- (NSMutableArray *)getAllTableName;
+/**
+ 删除表
+
+ @param tableName 删除的表名
+ @return 删除结果
+ */
+- (BOOL)deleteTable:(NSString *)tableName;
+/**
+ 根据数据库某一字段删除某一条记录
+
+ @param tableName 要删除的表名
+ @param sqlKey 要删除的字段名
+ @param keyWord 要删除的字段的对应值
+ @return 返回删除结果
+ */
+- (BOOL)deleTableOjb:(NSString *)tableName SQLKey:(NSString *)sqlKey KeyWord:(NSString*)keyWord;
+/**
+ 关闭数据库
+ */
+- (void)closeDB;
+
+
+/**
+ 查询所有该表名下的数据
+
+ @param tableName 表名
+ @return 返回查询结果{title： content：}
+ */
+- (NSDictionary *)queryAllQuickMenuWithTableName:(NSString *)tableName;
+
+@end
---
+++ SqliteSpec/Classes/DBSearchHisManager.m
@@ -0,0 +1,215 @@
+//
+//  DBManager.h
+//  YZDoctors
+//
+//  Created by lishengshu on 15-7-29.
+//  Copyright (c) 2015年 李胜书. All rights reserved.
+//
+
+#import "DBSearchHisManager.h"
+
+@implementation DBSearchHisManager
+
++ (DBSearchHisManager *)ShareInstance{
+    static DBSearchHisManager *sharedDBManagerInstance = nil;
+    static dispatch_once_t predicate;
+    dispatch_once(&predicate, ^{
+        sharedDBManagerInstance = [[self alloc] init];
+    });
+    return sharedDBManagerInstance;
+}
+#pragma mark - 创建并打开,创建位置在documents中
+- (BOOL)creatDatabase:(NSString *)dbName {
+    NSString *docsdir = [NSSearchPathForDirectoriesInDomains( NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    NSString *dbPath = [docsdir stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.sqlite",dbName]];
+    self.searchHisDB = [FMDatabase databaseWithPath:dbPath];
+    //为数据库设置缓存，提高查询效率
+    [self.searchHisDB setShouldCacheStatements:YES];
+    return [self.searchHisDB open];
+}
+
+- (BOOL)isDBReady {
+    if(!self.searchHisDB) {
+        return NO;
+    }
+    if (![self.searchHisDB open]) {
+       	return NO;
+    }
+    return YES;
+}
+#pragma mark-判断表是否存在,不存在则创建表
+- (BOOL)isTableExist:(NSString *)tName TKeyArr:(NSArray *)keyArr {
+    if (![self isDBReady])
+        return NO;
+    if(![self.searchHisDB tableExists:tName]) {
+        __block NSString *tempSql = @"";
+        [keyArr enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if (idx == 0) {
+                tempSql = [NSString stringWithFormat:@"(%@ INTEGER PRIMARY KEY AUTOINCREMENT",obj];
+            }else if(idx < keyArr.count - 1) {
+                tempSql = [NSString stringWithFormat:@"%@,%@ TEXT",tempSql,obj];
+            }else {
+                tempSql = [NSString stringWithFormat:@"%@,%@ TEXT)",tempSql,obj];
+            }
+        }];
+        NSString *sql = [NSString stringWithFormat:@"create table %@%@",tName,tempSql];
+        return [self.searchHisDB executeUpdate:sql];
+    }else {
+        //已经存在
+        return YES;
+    }
+}
+#pragma mark-插入列表 或者更新------------------------|*|*|*|*|*|
+- (BOOL)addTableObj:(NSString *)tableName DataDic:(NSDictionary *)dataDic {
+    NSArray *keyArr = [dataDic allKeys];
+    if ([self.searchHisDB tableExists:tableName]) {
+        NSString *sqlKey;
+        NSString *keyValue;
+        if (keyArr.count > 0) {
+            sqlKey = keyArr[0];
+            keyValue = dataDic[keyArr[0]];
+            FMResultSet *searchResult = [self SearchOne:tableName SQLKeyWord:sqlKey SearchKeyWords:keyValue];
+            int tempNumber = 0;
+            while ([searchResult next]) {
+                tempNumber++;
+                break;
+            }
+            if (tempNumber == 0) {
+                //不存在重复
+                __block NSString *keyString = @"";
+                __block NSString *valueString = @"";
+                if (keyArr.count == 1) {
+                    keyString = [NSString stringWithFormat:@"(%@)", keyArr[0]];
+                    valueString = [NSString stringWithFormat:@"('%@')", [dataDic[keyArr[0]] objConvertToJsonStr]];
+                } else {
+                    
+                    [keyArr enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                        if (idx == keyArr.count - 1) {
+                            keyString = [NSString stringWithFormat:@"%@ %@)",keyString,obj];
+                            valueString = [NSString stringWithFormat:@"%@'%@')",valueString,[dataDic[obj] objConvertToJsonStr]];
+                        }else {
+                            if ([keyString isEqualToString:@""]) {
+                                keyString = [NSString stringWithFormat:@"(%@,",obj];
+                                valueString = [NSString stringWithFormat:@"('%@',",[dataDic[obj] objConvertToJsonStr]];
+                            }else {
+                                keyString = [NSString stringWithFormat:@"%@ %@,",keyString,obj];
+                                valueString = [NSString stringWithFormat:@"%@'%@',",valueString,[dataDic[obj] objConvertToJsonStr]];
+                            }
+                        }
+                    }];
+                }
+                NSString *insertsql = [NSString stringWithFormat:@"INSERT INTO %@ %@ VALUES %@",tableName,keyString,valueString];
+                if ([self.searchHisDB executeUpdate:insertsql]) {
+                    //插入成功
+                    return YES;
+                }else {
+                    return NO;
+                }
+            }else {
+                __block NSString *tempString = @"";
+                [keyArr enumerateObjectsUsingBlock:^(NSString *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                    if (idx == 0) {
+                        tempString = [NSString stringWithFormat:@"%@ = '%@'",obj,[dataDic[obj] objConvertToJsonStr]];
+                    }else {
+                        tempString = [NSString stringWithFormat:@"%@,%@ = '%@'",tempString,obj,[dataDic[obj] objConvertToJsonStr]];
+                    }
+                }];
+//                NSString *updatesql = [NSString stringWithFormat:@"UPDATE %@ set %@ where %@ = '%@'",tableName,tempString,sqlKey,keyValue];
+                NSString *updatesql = [NSString stringWithFormat:@"UPDATE %@ set %@",tableName,tempString];
+
+                if ([self.searchHisDB executeUpdate:updatesql]) {
+                    return YES;
+                }else {
+                    return NO;
+                }
+            }
+        }
+    }
+    return NO;
+}
+#pragma mark - 查询数据
+#warning 取材方式
+//while ([messWithNumber next]) {
+//obj.mycontent = [messWithNumber stringForColumn:@"key"];
+- (FMResultSet *)SearchOne:(NSString *)tableName SQLKeyWord:(NSString *)sqlKeyWord SearchKeyWords:(NSString *)searchKeyWords {
+    FMResultSet *messWithNumber;
+//    NSString *searchsql = [NSString stringWithFormat:@"SELECT * FROM %@ where %@ = '%@'",tableName,sqlKeyWord,searchKeyWords];
+        NSString *searchsql = [NSString stringWithFormat:@"SELECT %@ FROM %@",sqlKeyWord, tableName];
+
+    if ([self.searchHisDB tableExists:tableName]) {
+        messWithNumber = [self.searchHisDB executeQuery:searchsql];
+    }
+    return messWithNumber;
+}
+
+- (FMResultSet *)SearchLastTen:(NSString *)tableName {
+    FMResultSet *messWithNumber;
+    NSString *searchsql=[NSString stringWithFormat:@"SELECT * FROM %@ order by SearchHisID DESC limit 0,10",tableName];
+    if ([self.searchHisDB tableExists:tableName]) {
+        messWithNumber = [self.searchHisDB executeQuery:searchsql];
+    }
+    return messWithNumber;
+}
+
+- (FMResultSet *)SearchAll:(NSString *)tableName {
+    FMResultSet *messWithNumber;
+    NSString *searchsql=[NSString stringWithFormat:@"SELECT * FROM %@",tableName];
+    if ([self.searchHisDB tableExists:tableName]) {
+        messWithNumber = [self.searchHisDB executeQuery:searchsql];
+    }
+    return messWithNumber;
+}
+#pragma mark - 获取所有表名
+- (NSMutableArray *)getAllTableName {
+    NSMutableArray *tableMessName = [NSMutableArray array];
+    FMResultSet  *tableNameSet;
+    NSString *searchsql=[NSString stringWithFormat:@"SELECT NAME FROM sqlite_master WHERE type='table' order by name"];
+    tableNameSet = [self.searchHisDB executeQuery:searchsql];
+    while ([tableNameSet next]) {
+        if (![[tableNameSet stringForColumn:@"name"] isEqualToString:@"sqlite_sequence"]) {
+            NSString *tableStringName = [tableNameSet stringForColumn:@"name"];
+            [tableMessName addObject:tableStringName];
+        }
+    }
+    return tableMessName;
+}
+#pragma mark - 删除表
+- (BOOL)deleteTable:(NSString *)tableName {
+    NSString *sqlstr = [NSString stringWithFormat:@"DROP TABLE %@", tableName];
+    if (![self.searchHisDB executeUpdate:sqlstr]) {
+        return NO;
+    }
+    return YES;
+}
+#pragma mark - 根据sqlKey删除数据
+- (BOOL)deleTableOjb:(NSString *)tableName SQLKey:(NSString *)sqlKey KeyWord:(NSString*)keyWord {
+    if ([self.searchHisDB tableExists:tableName]) {
+        NSString *insertsql = [NSString stringWithFormat:@"DELETE FROM %@ WHERE %@='%@'",tableName,sqlKey,keyWord];
+        return [self.searchHisDB executeUpdate:insertsql];
+    }
+    return NO;
+}
+#pragma mark - 关闭数据库
+- (void)closeDB {
+    [self.searchHisDB close];
+}
+
+#pragma mark - 查询所有
+- (NSDictionary *)queryAllQuickMenuWithTableName:(NSString *)tableName {
+    FMResultSet *result = [self SearchAll:tableName];
+    NSMutableDictionary *dic = @{}.mutableCopy;
+    while (result.next) {
+        
+        NSString *titleStr = [result stringForColumn:quickHelpTitle];
+        [dic setValue:[titleStr jsonStrConvertToObj] forKey:quickHelpTitle];
+        
+        NSString *contentStr = [result stringForColumn:quickHelpContent];
+        [dic setValue:[contentStr jsonStrConvertToObj] forKey:quickHelpContent];
+        
+        NSString *myQuickMenuStr = [result stringForColumn:myQuickMenu];
+        [dic setValue:[myQuickMenuStr jsonStrConvertToObj] forKey:myQuickMenu];
+    }
+    return dic;
+}
+
+@end
